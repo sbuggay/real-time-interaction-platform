16665e4ed24da81bdf426a901e28585539d9b0ae
sbuggay <sbuggay@gmail.com>
2013-10-16 15:07:54 -0500

    Update

--- rtip.js
+++ rtip.js
@@ -12,7 +12,21 @@ for(var i = 0; i < 10; i++) {
 var loop = setInterval(function() {
 
     c.fillStyle = "white";
-    c.fillRect(0, 0, 500, 500);
+    c.fillRect(0, 0, 512, 512);
+
+    for(var i = 0; i < 8; i++)
+    {
+        for(var j = 0; j < 8; j++)
+        {
+            if((i + j) % 2 == 0)
+                c.fillStyle="#444444";
+            else    
+                c.fillStyle="#AAAAAA";
+
+            c.fillRect(i * 64, j * 64, 64, 64); 
+        }
+
+    }
 
     for(var i = 0; i < 10; i++) {
         images[i].update();
@@ -23,10 +37,10 @@ var loop = setInterval(function() {
 var mouseX = 0, mouseY = 0;
 var mousePressed = false;
 canvas.mousemove(function(e) {
-  mouseX = e.offsetX;
-  mouseY = e.offsetY;
+    mouseX = e.offsetX;
+    mouseY = e.offsetY;
 })
- 
+
 $(document).mousedown(function(){
     mousePressed = true;
 }).mouseup(function(){
@@ -42,20 +56,21 @@ function DragImage(src, x, y) {
     var img = new Image();
     img.src = src;
     this.update = function() {
-        if (mousePressed){
+        if (mousePressed) {
             var left = that.x;
             var right = that.x + img.width;
             var top = that.y;
             var bottom = that.y + img.height;
             if (!drag){
-              startX = mouseX - that.x;
-              startY = mouseY - that.y;
+                startX = mouseX - that.x;
+                startY = mouseY - that.y;
             }
-            if (mouseX < right && mouseX > left && mouseY < bottom && mouseY > top){
-               drag = true;
+            if (mouseX < right && mouseX > left && mouseY < bottom && mouseY > top) {
+                drag = true;
             }
+
         }else{
-           drag = false;
+            drag = false;
         }
         if (drag){
             that.x = mouseX - startX;
